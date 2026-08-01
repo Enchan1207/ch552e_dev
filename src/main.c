add7704709981d537d7988696fe1927c541a1f35
enchan <enchant0130@gmail.com>
2026-08-01 18:04:31 +0900

    feat: delay関数の実装を修正, レジスタ定義を修正, IOモードをpush-pullに変更

--- src/main.c
+++ src/main.c
@@ -4,24 +4,29 @@
 __sfr __at(0x93) P1_DIR_PU;
 
 // P1 port output mode register
-__sfr __at(0x93) P1_MOD_OC;
+__sfr __at(0x92) P1_MOD_OC;
 
 // P1 port input and output register
 __sfr __at(0x90) P1;
 
-void delay() {
-    uint32_t i = 0;
+#define P1_4_MASK (1u << 4)
+
+void delay(void) {
+    volatile uint16_t i = 0;
     while (--i) {
     }
 }
 
 int main(void) {
-    // setup
+    // P1.4 をpush-pull出力にする
+    P1_MOD_OC &= ~P1_4_MASK;
+    P1_DIR_PU |= P1_4_MASK;
 
     while (1) {
-        P1 = 0b00010000;
+        P1 |= P1_4_MASK;
         delay();
-        P1 = 0b00000000;
+
+        P1 &= ~P1_4_MASK;
         delay();
     }
 }
