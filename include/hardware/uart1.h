d6168d1061617a08bf33bae6d2de8e761e2d3879
enchan <enchant0130@gmail.com>
2026-08-13 09:58:15 +0900

    feat: UART1ドライバを移植

---
+++ include/hardware/uart1.h
@@ -0,0 +1,68 @@
+#ifndef HARDWARE_UART1_H
+#define HARDWARE_UART1_H
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#ifndef UART1_RX_BUFSIZE
+/// @brief 受信バッファサイズ
+#define UART1_RX_BUFSIZE 32
+#endif
+
+#if (UART1_RX_BUFSIZE & (UART1_RX_BUFSIZE - 1)) != 0
+#error "UART1_RX_BUFSIZE must be a power of 2"
+#endif
+
+#ifndef UART1_BAUD_RATE
+/// @brief UART1ボーレート
+#define UART1_BAUD_RATE 9600
+#endif
+
+/// @brief UART1コンテキスト
+typedef struct uart1_ctx_t uart1_ctx_t;
+
+/// @brief UART1
+extern uart1_ctx_t* uart1;
+
+/// @brief UART1通信開始
+/// @param ctx
+/// @note ボーレートはマクロ UART_BAUD_RATE により設定します。
+void uart_begin(void);
+
+/// @brief UART送信 (バイナリデータ)
+/// @param ctx
+/// @param data
+/// @param len
+/// @note 与えられたデータの送信が完了するまでブロックしま
+void uart_write(uart1_ctx_t* ctx, const uint8_t* const data, size_t len);
+
+/// @brief USART送信 (バイナリデータ, ノンブロッキング)
+/// @param ctx
+/// @param data
+/// @param len
+/// @note 現在実行中の送信がある場合は、完了するまでブロックします。
+void uart_write_noblock(uart1_ctx_t* ctx, const uint8_t* const data, size_t len);
+
+/// @brief USART送信 (文字列)
+/// @param ctx
+/// @param str
+/// @note 与えられたデータの送信が完了するまでブロックします。
+void uart_print(uart1_ctx_t* ctx, const char* const str);
+
+/// @brief USART送信 (文字列, ノンブロッキング)
+/// @param ctx
+/// @param str
+/// @note 現在実行中の送信がある場合は、完了するまでブロックします。
+void uart_print_noblock(uart1_ctx_t* ctx, const char* const str);
+
+/// @brief USART受信
+/// @return 受信結果
+/// @note データを受信するまでブロックします。
+uint8_t uart_read(uart1_ctx_t* ctx);
+
+/// @brief 受信バッファ状態確認
+/// @return データを受信していれば true が返ります。
+bool uart_available(uart1_ctx_t* ctx);
+
+#endif /* HARDWARE_UART1_H */
---
+++ src/hardware/uart1.c
@@ -0,0 +1,116 @@
+#include "hardware/uart1.h"
+
+#include "ch552e/interrupt.h"
+#include "ch552e/io.h"
+
+struct uart1_ctx_t {
+    volatile bool txBusy;
+    const uint8_t* volatile txDataPtr;
+    volatile size_t txRemaining;
+
+    volatile uint8_t rxBuffer[UART1_RX_BUFSIZE];
+    volatile uint8_t rxHead;
+    volatile uint8_t rxTail;
+};
+
+static uart1_ctx_t uart1_internal = {
+    .txBusy = false,
+    .txDataPtr = NULL,
+    .txRemaining = 0,
+
+    .rxBuffer = {0},
+    .rxHead = 0,
+    .rxTail = 0,
+};
+
+uart1_ctx_t* uart1 = &uart1_internal;
+
+// UART1割込み
+ISR(INT_NO_UART1) {
+    // 受信完了
+    if (U1RI) {
+        uint8_t data = SBUF1;
+        uint8_t nextTail = (uart1->rxTail + 1) & (UART1_RX_BUFSIZE - 1);
+
+        // バッファがいっぱいの場合は読み捨て
+        if (nextTail == uart1->rxHead) {
+            return;
+        }
+
+        uart1->rxBuffer[uart1->rxTail] = data;
+        uart1->rxTail = nextTail;
+
+        U1RI = 0;
+    }
+
+    // 送信完了
+    if (U1TI) {
+        if (uart1->txRemaining > 0) {
+            SBUF1 = *uart1->txDataPtr;
+            uart1->txDataPtr++;
+            uart1->txRemaining--;
+        } else {
+            uart1->txBusy = false;
+        }
+
+        U1TI = 0;
+    }
+}
+
+void uart_begin(void) {
+    irq_disable();
+
+    SBAUD1 = 0;
+
+    // 8/N/1, 倍速, 受信有効, 受信割込み有効
+    U1SM0 = 0;
+    U1SMOD = 1;
+    U1REN = 1;
+
+    // TODO: UART1_BAUD_RATEから計算する
+    IE_UART1 = 217;
+}
+
+void uart_write(uart1_ctx_t* ctx, const uint8_t* const data, size_t len) {
+    uart_write_noblock(ctx, data, len);
+    while (ctx->txBusy);
+}
+
+void uart_write_noblock(uart1_ctx_t* ctx, const uint8_t* const data, size_t len) {
+    while (ctx->txBusy);
+
+    ctx->txDataPtr = data;
+    ctx->txRemaining = len;
+    ctx->txBusy = true;
+
+    // 最初の1byteを書いておく
+    SBUF1 = *uart1->txDataPtr;
+}
+
+void uart_print(uart1_ctx_t* ctx, const char* const str) {
+    uart_print_noblock(ctx, str);
+
+    while (ctx->txRemaining > 0);
+}
+
+void uart_print_noblock(uart1_ctx_t* ctx, const char* const str) {
+    size_t len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    uart_write_noblock(ctx, (const uint8_t*)str, len);
+}
+
+uint8_t uart_read(uart1_ctx_t* ctx) {
+    while (ctx->rxHead == ctx->rxTail);
+
+    uint8_t data = ctx->rxBuffer[ctx->rxHead];
+    ctx->rxHead = (ctx->rxHead + 1) & (UART1_RX_BUFSIZE - 1);
+
+    return data;
+}
+
+bool uart_available(uart1_ctx_t* ctx) {
+    return ctx->rxHead != ctx->rxTail;
+}
