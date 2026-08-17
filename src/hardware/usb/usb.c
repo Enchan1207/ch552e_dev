306af3e6a66bd55f06044f33b17c8b59880e6b3c
enchan <enchant0130@gmail.com>
2026-08-18 08:40:46 +0900

    chore: ISRを簡略化

--- src/hardware/usb/usb.c
+++ src/hardware/usb/usb.c
@@ -1,8 +1,9 @@
+#include "hardware/usb/usb.h"
+
 #include <ch552e/io.h>
 #include <ch552e/memory.h>
 #include <stddef.h>
 
-#include "hardware/usb/usb.h"
 #include "usb_private.h"
 
 usb_ctx_t ctx_internal;
@@ -50,39 +51,34 @@ void usb_reset_context(void) {
     ctx_internal.tx_offset = 0;
 }
 
-static inline void usb_handle_bus_reset(void) {
-    usb_reset_context();
-    USB_DEV_AD = 0x00;
-    UEP0_T_LEN = 0x00;
-    UEP0_CTRL = UEP_R_RES_ACK | UEP_T_RES_NAK;
-}
-
-static inline void usb_handle_transfer(uint8_t status, uint8_t length) {
-    uint8_t endpoint = status & MASK_UIS_ENDP;
-    uint8_t token = status & MASK_UIS_TOKEN;
-
-    if (endpoint == 0) {
-        usb_ep0_handle_packet(&ctx_internal, token, length);
-    }
-}
-
-static inline void usb_handle_suspend(void) {
-    // NOTE 現時点では特に何もしない
-}
-
 ISR(INT_NO_USB) {
     if (UIF_TRANSFER) {
-        usb_handle_transfer(USB_INT_ST, USB_RX_LEN);
+        uint8_t status = USB_INT_ST;
+        uint8_t length = USB_RX_LEN;
+
+        uint8_t endpoint = status & MASK_UIS_ENDP;
+        uint8_t token = status & MASK_UIS_TOKEN;
+
+        if (endpoint == 0) {
+            usb_ep0_handle_packet(&ctx_internal, token, length);
+        }
+
+        // TODO: EP1-4のパケットを処理する
+
         UIF_TRANSFER = 0;
     }
 
     if (UIF_BUS_RST) {
-        usb_handle_bus_reset();
+        usb_reset_context();
+
+        USB_DEV_AD = 0x00;
+        UEP0_T_LEN = 0x00;
+        UEP0_CTRL = UEP_R_RES_ACK | UEP_T_RES_NAK;
         UIF_BUS_RST = 0;
     }
 
     if (UIF_SUSPEND) {
-        usb_handle_suspend();
+        // NOTE: 現時点では特に何もしない
         UIF_SUSPEND = 0;
     }
 }
