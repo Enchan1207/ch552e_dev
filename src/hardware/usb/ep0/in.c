c59f021d7131b075a6e0ddecaef5fd3e4b801bf8
enchan <enchant0130@gmail.com>
2026-08-18 11:48:20 +0900

    feat: #7 IN/OUTパケットの処理を追加

---
+++ src/hardware/usb/ep0/in.c
@@ -0,0 +1,23 @@
+#include "internal.h"
+
+void usb_ep0_handle_in(usb_ep0_ctx_t* ctx) {
+    switch (ctx->state) {
+        case USB_STATE_WAIT_DEVICE_ADDRESS:
+            USB_DEV_AD = ctx->address_pending.address_candidate;
+            ctx->state = USB_STATE_WAIT_STATUS_OUT;
+
+            UEP0_T_LEN = 0x00;
+            UEP0_CTRL = UEP_R_RES_ACK | UEP_T_RES_NAK;
+            break;
+
+        case USB_STATE_GET_DEVICE_DESCRIPTOR:
+            ctx->state = USB_STATE_WAIT_STATUS_OUT;
+
+            UEP0_T_LEN = 0x00;
+            UEP0_CTRL = UEP_R_RES_ACK | UEP_T_RES_NAK;
+            return;
+
+        default:
+            break;
+    }
+}
