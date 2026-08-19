3912d6a12392802f7bc317854397c8ae8c1c31a2
enchan <enchant0130@gmail.com>
2026-08-19 21:28:34 +0900

    chore: IRQ制御をmainに移動

--- src/main.c
+++ src/main.c
@@ -83,6 +83,8 @@ const __code uint8_t* usb_get_interface_child_descriptor(uint8_t if_index, uint8
 }
 
 int main(void) {
+    irq_disable();
+
     usb_setup_fifo_item_t setup_packet;
 
     // クロック設定 (内蔵オシレータ, 6MHz)
