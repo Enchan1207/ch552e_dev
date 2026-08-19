4294bdb3374b73c9b1d6614a587959c53555ec7f
enchan <enchant0130@gmail.com>
2026-08-19 11:18:24 +0900

    fix: #11 ISR重複を削除

--- src/hardware/uart1/uart1.c
+++ src/hardware/uart1/uart1.c
@@ -24,36 +24,6 @@ __idata uart1_ctx_t uart1 = {
 __xdata __at(UART1_BUFFER_ADDRESS)
 uint8_t rx_buffer[UART1_RX_BUFSIZE];
 
-// UART1割込み
-ISR(INT_NO_UART1) {
-    // 受信完了
-    if (U1RI) {
-        uint8_t data = SBUF1;
-        uint8_t nextTail = (uart1.rx_tail + 1) & (UART1_RX_BUFSIZE - 1);
-
-        // バッファがいっぱいの場合は読み捨て
-        if (nextTail != uart1.rx_head) {
-            rx_buffer[uart1.rx_tail] = data;
-            uart1.rx_tail = nextTail;
-        }
-
-        U1RI = 0;
-    }
-
-    // 送信完了
-    if (U1TI) {
-        U1TI = 0;
-
-        if (uart1.tx_remaining > 0) {
-            SBUF1 = *uart1.tx_data_ptr;
-            uart1.tx_data_ptr++;
-            uart1.tx_remaining--;
-        } else {
-            uart1.tx_busy = false;
-        }
-    }
-}
-
 void uart_begin(void) {
     irq_disable();
 
